90418e1e66d903744986b14c70e571a7d1b0fcd2
Sakshyam Subedi <sakshyamsubedi88@gmail.com>
2025-12-02 20:45:52 +0545

    Smooth scroll behaviour and profile pic

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('active');
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
